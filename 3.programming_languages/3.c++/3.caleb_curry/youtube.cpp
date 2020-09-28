0fffc9e324f11a8f2de078c4d9d1a6cb75ac0704
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2020-09-28 19:37:08 +0600

    updated!

--- 3.programming_languages/3.c++/3.caleb_curry/youtube.cpp
+++ 3.programming_languages/3.c++/3.caleb_curry/youtube.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
+
+// using namespace std; // using directive! (bad practice when your project is huge!)
+using std::cout; // good practice! specific and avoid naming conflict!
+using std::endl; // good practice! specific and avoid naming conflict!
+
 int main() // main function
 {
-    std::cout << "Hello World!\n";
+    // std::cout << "Hello World!\n";
+    // std::cout << "Hello World!" << std::endl;
+    cout << "Hello World!\n";
+    cout << "Hello World!" << endl;
     return 0;
 }
