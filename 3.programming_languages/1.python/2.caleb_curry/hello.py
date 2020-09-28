0ff7aac87a02b6a88b7a4565510534967bc09cab
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2020-09-28 21:04:17 +0600

    updated!

--- 3.programming_languages/1.python/2.caleb_curry/hello.py
+++ 3.programming_languages/1.python/2.caleb_curry/hello.py
@@ -1,2 +1,7 @@
-print("Hello World!")
-print(5 + 5, "hey", 10.3, "hi!", end="\n")
+# print("Hello World!")
+# print(5 + 5, "hey", 10.3, "hi!", end="\n")
+
+age = 5
+print(age)
+val = 10 / 5 + 3 * 3
+print(val)
