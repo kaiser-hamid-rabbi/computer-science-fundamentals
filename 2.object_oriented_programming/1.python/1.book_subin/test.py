cceb4dbaca8e31494d4b54968e45a31cffce29e4
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2020-09-30 23:11:56 +0600

    updated!

--- 2.object_oriented_programming/1.python/1.book_subin/test.py
+++ 2.object_oriented_programming/1.python/1.book_subin/test.py
@@ -1,2 +1,20 @@
-import builtins
-print(dir(builtins))
+# import builtins
+# print(dir(builtins))
+
+# import datetime
+# today = datetime.date.today()
+# print(today)
+# today = datetime.datetime.today()
+# print(today)
+
+# from datetime import date
+# d = date.today()
+# print(d)
+
+# from datetime import datetime
+# d = datetime.today()
+# print(d)
+
+import webbrowser as wb
+url = "https://github.com/kaiser-hamid-rabbi"
+wb.open(url)
