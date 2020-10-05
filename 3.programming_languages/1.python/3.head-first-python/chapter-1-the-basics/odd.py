0aca28dfc0c70233bdf100220666f74da26d1272
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2020-10-05 13:46:27 +0600

    updated!

--- 3.programming_languages/1.python/3.head-first-python/chapter-1-the-basics/odd.py
+++ 3.programming_languages/1.python/3.head-first-python/chapter-1-the-basics/odd.py
@@ -11,11 +11,24 @@
 
 
 
-from os import getcwd
+# from os import getcwd
 
-where_am_I = getcwd()
-print("I'm in:" ,where_am_I)
+# where_am_I = getcwd()
+# print("I'm in:" ,where_am_I)
 
-import sys
-print("My Operating System:", sys.platform)
-print("Python version:", sys.version)
+# import sys
+# print("My Operating System:", sys.platform)
+# print("Python version:", sys.version)
+
+
+
+from datetime import datetime
+
+odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
+
+right_this_minute = datetime.today().minute
+
+if right_this_minute in odds:
+    print("This minute seems a little odd!")
+else:
+    print("Not an odd minute!")
