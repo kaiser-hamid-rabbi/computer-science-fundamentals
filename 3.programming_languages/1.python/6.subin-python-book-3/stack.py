ebc5ec1046a7d9e54bc13be1cc5ee8ba4becc010
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2020-10-10 00:46:10 +0600

    updated!

---
+++ 3.programming_languages/1.python/6.subin-python-book-3/stack.py
@@ -0,0 +1,29 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+        print(f"{item} is inserted in the Stack!")
+
+    def pop(self):
+        ret = self.items.pop()
+        print(f"{ret} is deleted from the Stack!")
+        return ret
+
+    def is_empty(self):
+        if self.items == []:
+            return True
+        return False
+
+if __name__ == "__main__":
+    
+    s = Stack()
+    
+    s.push(1)
+    s.push(2)
+    s.push(3)
+
+    while not s.is_empty():
+        item = s.pop()
+        # print(item)
