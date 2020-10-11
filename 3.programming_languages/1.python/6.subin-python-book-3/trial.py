2a636c2f7ed4c0103aea158b203070b553cee4eb
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2020-10-11 14:50:11 +0600

    updated!

--- 3.programming_languages/1.python/6.subin-python-book-3/trial.py
+++ 3.programming_languages/1.python/6.subin-python-book-3/trial.py
@@ -1,2 +1,24 @@
 class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        pass
+
+    def pop(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+if __name__ == "__main__":
+
+    s = Stack()
     
+    s.push(1)
+    s.push(2)
+    s.push(3)
+
+    while not s.is_empty():
+        item = s.pop()
+        print(item)
