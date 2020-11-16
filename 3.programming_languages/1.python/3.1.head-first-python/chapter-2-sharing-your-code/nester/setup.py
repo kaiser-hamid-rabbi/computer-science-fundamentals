2dd03c842f6075d0f44f07a97f1ba7265976089a
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2020-11-16 20:14:56 +0600

    updated!

---
+++ 3.programming_languages/1.python/3.1.head-first-python/chapter-2-sharing-your-code/nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'Kaiser Hamid Rabbi',
+    author_email = 'kaiser.hamid.rabbi@gmail.com',
+    url = 'https://kaiser-hamid-rabbi.github.io/',
+    description = 'A simple printer of nested lists'
+    )
