1c05f5ee5ef6afad7bd0ab182e6119020dd6c9af
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2020-11-30 11:11:12 +0600

    updated!

--- 3.programming_languages/1.python/3.1.head-first-python/chapter-2-sharing-your-code/nester/setup.py
+++ 3.programming_languages/1.python/3.1.head-first-python/chapter-2-sharing-your-code/nester/setup.py
@@ -6,6 +6,6 @@ setup(
     py_modules = ['nester'],
     author = 'Kaiser Hamid Rabbi',
     author_email = 'kaiser.hamid.rabbi@gmail.com',
-    url = 'https://kaiser-hamid-rabbi.github.io/',
+    url = 'https://github.com/kaiser-hamid-rabbi/computer-science-fundamentals/tree/master/3.programming_languages/1.python/3.1.head-first-python/chapter-2-sharing-your-code/nester',
     description = 'A simple printer of nested lists'
     )
