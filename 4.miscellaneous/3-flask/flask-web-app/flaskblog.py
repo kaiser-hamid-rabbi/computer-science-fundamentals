02e1c88e97b54e8ecb155c11efe8556edf9810ab
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2020-12-25 18:39:28 +0600

    updated!

--- 4.miscellaneous/3-flask/flask-web-app/flaskblog.py
+++ 4.miscellaneous/3-flask/flask-web-app/flaskblog.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '370275fa546d7c8f72daa0d2ce68a6c2'
 
 posts = [
     {
@@ -24,6 +28,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
