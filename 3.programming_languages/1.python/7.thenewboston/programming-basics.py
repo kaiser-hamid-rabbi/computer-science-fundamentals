7a872343dadca7f8e93f8ba2990634f3db8682af
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-06-11 08:15:31 +0600

    updated!

---
+++ 3.programming_languages/1.python/7.thenewboston/programming-basics.py
@@ -0,0 +1,85 @@
+# # ------------------------ Numbers ------------------------------------
+# >>> 3 + 4
+# 7
+# >>> 3 * 20
+# 60
+# >>> 12 / 4
+# 3.0
+# >>> 8 + 2 * 10
+# 28
+# >>> (8 + 2) * 10
+# 100
+# >>> 18 / 4
+# 4.5
+# >>> 18 // 4
+# 4
+# >>> 18 % 4
+# 2
+# >>> 5 * 5 * 5
+# 125
+# >>> 5 ** 3
+# 125
+# >>> tuna = 5
+# >>> 20 + tuna
+# 25
+# >>> bacon = 18
+# >>> bacon / tuna
+# 3.6
+
+
+# # ------------------------ Strings ------------------------------------
+# >>> Bucky Roberts
+#   File "<stdin>", line 1
+#     Bucky Roberts
+#           ^
+# SyntaxError: invalid syntax
+# >>> "Bucky Roberts"
+# 'Bucky Roberts'
+# >>> 'Bucky Roberts is awesome!'
+# 'Bucky Roberts is awesome!'
+# >>> 'I don't think shes 18'
+#   File "<stdin>", line 1
+#     'I don't think shes 18'
+#            ^
+# SyntaxError: invalid syntax
+# >>> "I don't think shes 18"
+# "I don't think shes 18"
+# >>> 'I don\'t think shes 18'
+# "I don't think shes 18"
+# >>> 'She said, "What part of the cow is the meatloaf from?"'
+# 'She said, "What part of the cow is the meatloaf from?"'
+# >>> print("hey now brown cow")
+# hey now brown cow
+# >>> print('C:\Bucky\Desktop\nudePics')
+# C:\Bucky\Desktop
+# udePics
+# >>> print(r'C:\Bucky\Desktop\nudePics')
+# C:\Bucky\Desktop\nudePics
+# >>> firstName = "Bucky "
+# >>> firstName + "Roberts"
+# 'Bucky Roberts'
+# >>> firstName + "McLovin"
+# 'Bucky McLovin'
+# >>> firstName * 5
+# 'Bucky Bucky Bucky Bucky Bucky '
+
+
+# # ------------------------ Numbers ------------------------------------
+
+# # ------------------------ Numbers ------------------------------------
+
+# # ------------------------ Numbers ------------------------------------
+
+# # ------------------------ Numbers ------------------------------------
+
+# # ------------------------ Numbers ------------------------------------
+
+# # ------------------------ Numbers ------------------------------------
+
+# # ------------------------ Numbers ------------------------------------
+
+# # ------------------------ Numbers ------------------------------------
+
+# # ------------------------ Numbers ------------------------------------
+
+# # ------------------------ Numbers ------------------------------------
