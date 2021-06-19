4525578bd6a1f18544f2719296835706901eac34
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-06-20 04:54:36 +0600

    updated!

---
+++ 3.programming_languages/3.c++/7.the-c++-programming-language/chapter-2/main.cpp
@@ -0,0 +1,283 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    cout << "Hello, world!" << "\n";
+//}
+
+
+
+//#include <iostream>
+//
+//int main()
+//{
+//    std::cout << "Hello, world!" << "\n";
+//}
+
+
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//double square(double x)
+//{
+//    return x * x;
+//}
+//
+//void print_square(double x)
+//{
+//    cout << "the square of " << x << " is: " << square(x) << "\n";
+//}
+//
+//int main()
+//{
+//    print_square(1.234);
+//}
+
+
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    double a = 2.3;
+//    double b {2.3};
+//
+//    cout << "a: " << a << " b: " << b << "\n";
+//
+//    int c = 2.3; // it becomes 2
+////    int d {2.3}; // make error
+//
+//    cout << "c: " << c << "\n";
+//}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << "\n";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
