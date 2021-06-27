64cd99624549c27911b5afd0a5706cbf871583e8
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-06-27 20:27:53 +0600

    updated!

--- 2.data_structures_and_algorithms/0.object_oriented_programming/1.c++/ProgrammingKnowledge/oop/main.cpp
+++ 2.data_structures_and_algorithms/0.object_oriented_programming/1.c++/ProgrammingKnowledge/oop/main.cpp
@@ -151,37 +151,266 @@ Destructors in C++
 =====================================================
 */
 
+//#include <iostream>
+//#include <string>
+//
+//class Books
+//{
+//private:
+//    std::string name;
+//public:
+//    Books(std::string x) // Constructor with parameter
+//    {
+//        std::cout << "You are in Constructor with parameter!" << "\n";
+//        setName(x);
+//    }
+//    ~Books() // Destructor without parameter
+//    {
+//        std::cout << "You are in Destructor without parameter!" << "\n";
+//    }
+//    void setName(std::string x) // Setter
+//    {
+//        name = x;
+//    }
+//    std::string getName() // Getter
+//    {
+//        return name;
+//    }    
+//};
+//
+//int main()
+//{
+//    Books book1("C++ Tutorial");
+//    std::cout << book1.getName() << "\n";
+//    Books book2("Python Tutorial");
+//    std::cout << book2.getName() << "\n";
+//}
+
+
+
+/* 
+=====================================================
+Operator Overloading in C++
+=====================================================
+*/
+
+//#include <iostream>
+//
+////(4, 2) and (2, 2)
+////(4 + 2, 2 + 2) =  (6, 4)
+//
+//class Vector
+//{
+//public:
+//    int x, y;
+//    Vector () {};
+//    Vector (int a, int b)
+//    {
+//        x = a;
+//        y = b;
+//    }
+//    Vector operator+(const Vector&);
+//};
+//
+//Vector Vector::operator+(const Vector& parameter)
+//{
+//    Vector temp;
+//    temp.x = x + parameter.x;
+//    temp.y = y + parameter.y;
+//    return temp;
+//}
+//
+//int main()
+//{
+//    Vector vec1(4, 2);
+//    Vector vec2(2, 2);
+//    Vector result;
+//    result = vec1 + vec2;
+//    std::cout << "The result is ("<< result.x << ", " << result.y << ")" << "\n";
+//}
+
+
+
+/* 
+=====================================================
+C++ Inheritance
+=====================================================
+*/
+
+//#include <iostream>
+//#include <string>
+//
+//// [rectangle/ triange/ square] [is a] polygon
+//// [dog/ cat/ cow] [is an] animal
+//// [bugatti/ mclaren/ porsche] [is a] car
+//class Polygon // [Base/Parent] Class
+//{
+//public:
+//    void setValues(int a, int b)
+//    {
+//        width = a;
+//        height = b;
+//    }
+//protected:
+//    int height;
+//    int width;
+//};
+//
+//class Rectangle : public Polygon // [Derived/Child] Class
+//{
+//public:
+//    int area()
+//    {
+//        return (height * width);
+//    }
+//};
+//
+//class Triangle : public Polygon // [Derived/Child] Class
+//{
+//public:
+//    int area()
+//    {
+//        return (height * width / 2);
+//    }
+//};
+//
+//class Square : public Polygon // [Derived/Child] Class
+//{
+//public:
+//    int area()
+//    {
+//        return (height * width);
+//    }
+//};
+//
+//int main()
+//{
+//    Rectangle rec;
+//    Triangle tri;
+//    Square sqr;
+//    rec.setValues(15, 10);
+//    tri.setValues(15, 10);
+//    sqr.setValues(10, 10);
+//    
+//    std::cout << "Area of Rectangle is: " << rec.area() << "\n";
+//    std::cout << "Area of Triangle is: " << tri.area() << "\n";
+//    std::cout << "Area of Square is: " << sqr.area() << "\n";
+//}
+
+
+
+/* 
+=====================================================
+C++ Multiple Inheritance
+=====================================================
+*/
+
+//#include <iostream>
+//#include <string>
+//
+//// [rectangle/triange/square] [is a] polygon
+//// [dog/cat/cow] [is an] animal
+//// [bugatti/mclaren/porsche] [is a] car
+//class Polygon // [Base/Parent] Class
+//{
+//public:
+//    void setValues(int a, int b)
+//    {
+//        width = a;
+//        height = b;
+//    }
+//protected:
+//    int height;
+//    int width;
+//};
+//
+//class Description // Another [Base/Parent] Class
+//{
+//public:
+//    void print_description(std::string des)
+//    {
+//        std::cout << "We are using " << des << " class's object\n";
+//    }
+//};
+//
+//class Rectangle : public Polygon, public Description// [Derived/Child] Class
+//{
+//public:
+//    int area()
+//    {
+//        return (height * width);
+//    }
+//};
+//
+//class Triangle : public Polygon, public Description // [Derived/Child] Class
+//{
+//public:
+//    int area()
+//    {
+//        return (height * width / 2);
+//    }
+//};
+//
+//class Square : public Polygon, public Description // [Derived/Child] Class
+//{
+//public:
+//    int area()
+//    {
+//        return (height * width);
+//    }
+//};
+//
+//int main()
+//{
+//    Rectangle rec;
+//    Triangle tri;
+//    Square sqr;
+//    rec.setValues(15, 10);
+//    tri.setValues(15, 10);
+//    sqr.setValues(10, 10);
+//    
+//    // Base: Polygon
+//    std::cout << "Area of Rectangle is: " << rec.area() << "\n";
+//    std::cout << "Area of Triangle is: " << tri.area() << "\n";
+//    std::cout << "Area of Square is: " << sqr.area() << "\n";
+//    
+//    // Base: Polygon and Description
+//    rec.print_description("Rectangle");
+//    tri.print_description("Triangle");
+//    sqr.print_description("Square");
+//}
+
+
+
+/* 
+=====================================================
+Friend Class in C++
+=====================================================
+*/
+
 #include <iostream>
 #include <string>
 
-class Books
+class MyClass
 {
+    friend class MyAnotherClass;
 private:
-    std::string name;
+    int secret = 10;
+};
+
+class MyAnotherClass
+{
 public:
-    Books(std::string x) // Constructor with parameter
+    void showSecret(MyClass mc)
     {
-        std::cout << "You are in Constructor with parameter!" << "\n";
-        setName(x);
+        mc.secret++;
+        std::cout << mc.secret << "\n";
     }
-    ~Books() // Destructor without parameter
-    {
-        std::cout << "You are in Destructor without parameter!" << "\n";
-    }
-    void setName(std::string x) // Setter
-    {
-        name = x;
-    }
-    std::string getName() // Getter
-    {
-        return name;
-    }    
 };
 
 int main()
 {
-    Books book1("C++ Tutorial");
-    std::cout << book1.getName() << "\n";
-    Books book2("Python Tutorial");
-    std::cout << book2.getName() << "\n";
+    MyClass mc;
+    MyAnotherClass mac;
+    mac.showSecret(mc);
 }
