b350121a7193df2ac251a75560a7bd5065decd6b
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-06-28 21:14:22 +0600

    updated!

--- 2.data_structures_and_algorithms/0.object_oriented_programming/1.c++/CodeBeauty/OOP/main.cpp
+++ 2.data_structures_and_algorithms/0.object_oriented_programming/1.c++/CodeBeauty/OOP/main.cpp
@@ -253,7 +253,116 @@ OOP Abstraction
 
 /*
 =====================================================
-OOP Abstraction
+OOP Inheritance
+=====================================================
+*/
+
+//#include <iostream>
+//
+//class AbstractEmployee // Abstract Class which implemented a contract.
+//{
+//    virtual void AskForPromotion() = 0; // pure virtual function
+//};
+//
+//class Employee : AbstractEmployee
+//{
+//private:
+//    std::string Company;
+//    int Age;
+//protected:
+//    std::string Name;
+//public:
+//    void setName(std::string name) // setter
+//    {
+//        Name = name;
+//    }
+//    std::string getName() // getter
+//    {
+//        return Name;
+//    }
+//    void setCompany(std::string company) // setter
+//    {
+//        Company = company;
+//    }
+//    std::string getCompany() // getter
+//    {
+//        return Company;
+//    }
+//    void setAge(int age) // setter
+//    {
+//        if(age >= 18) Age = age; // set constraints for defining a variable
+//        else std::cout << "Sorry! You are not eligible. You must be 18 years old.\n";
+//    }
+//    int getAge() // getter
+//    {
+//        return Age;
+//    }
+//
+//    void IntroduceYourself() // class method
+//    {
+//        std::cout << "Name: " << Name << "\n";
+//        std::cout << "Company: " << Company << "\n";
+//        std::cout << "Age: " << Age << "\n";
+//    }
+//    Employee(std::string name, std::string company, int age) // Constructor - use for initialization
+//    {
+//        Name = name;
+//        Company = company;
+//        Age = age;
+//    }
+//    void AskForPromotion() // Pure Virtual Function from AbstractEmployee class
+//    {
+//        if(Age > 30) std::cout << Name << " got promoted!\n";
+//        else std::cout << Name << ", sorry NO promotion for you!\n";
+//    }
+//};
+//
+//class Developer : public Employee
+//{
+//public:
+//    std::string FavProgrammingLanguage;
+//    Developer(std::string name, std::string company, int age, std::string favProgrammingLanguage) : Employee(name, company, age) // constructor with parameters and inherits Employee class's constructor
+//    {
+//        FavProgrammingLanguage = favProgrammingLanguage;
+//    }
+//    void FixBug()
+//    {
+//        std::cout << Name << " fixed bug using " << FavProgrammingLanguage << ".\n";
+//    }
+//};
+//
+//class Teacher : public Employee
+//{
+//public:
+//    std::string Subject;
+//    void PrepareLesson()
+//    {
+//        std::cout << Name << " is preparing " << Subject << " lesson.\n";
+//    }
+//    Teacher(std::string name, std::string company, int age, std::string subject) : Employee(name, company, age)
+//    {
+//        Subject = subject;
+//    }
+//};
+//
+//int main()
+//{
+//    Developer dev = Developer("Kaiser", "TigerIT", 25, "C++");
+//    dev.IntroduceYourself(); // from Employee
+//    dev.FixBug(); // from Developer
+//    dev.AskForPromotion(); // from abstract class
+//
+//    Teacher tea = Teacher("Corey", "Google", 35, "Math");
+//    tea.IntroduceYourself(); // from Employee
+//    tea.PrepareLesson(); // from Teacher
+//    tea.AskForPromotion(); // from abstract class
+//}
+
+
+
+/*
+=====================================================
+OOP Polymorphisam
 =====================================================
 */
 
@@ -267,9 +376,10 @@ class AbstractEmployee // Abstract Class which implemented a contract.
 class Employee : AbstractEmployee
 {
 private:
-    std::string Name;
     std::string Company;
     int Age;
+protected:
+    std::string Name;
 public:
     void setName(std::string name) // setter
     {
@@ -314,14 +424,67 @@ public:
         if(Age > 30) std::cout << Name << " got promoted!\n";
         else std::cout << Name << ", sorry NO promotion for you!\n";
     }
+    virtual void Work() // Virtual Function
+    {
+        std::cout << Name << " is checking email, task backlog, performing tasks...\n";
+    }
+};
+
+class Developer : public Employee
+{
+public:
+    std::string FavProgrammingLanguage;
+    Developer(std::string name, std::string company, int age, std::string favProgrammingLanguage) : Employee(name, company, age) // constructor with parameters and inherits Employee class's constructor
+    {
+        FavProgrammingLanguage = favProgrammingLanguage;
+    }
+    void FixBug()
+    {
+        std::cout << Name << " fixed bug using " << FavProgrammingLanguage << ".\n";
+    }
+    void Work()
+    {
+        std::cout << Name << " is writing " << FavProgrammingLanguage << " code.\n";
+    }
+};
+
+class Teacher : public Employee
+{
+public:
+    std::string Subject;
+    void PrepareLesson()
+    {
+        std::cout << Name << " is preparing " << Subject << " lesson.\n";
+    }
+    Teacher(std::string name, std::string company, int age, std::string subject) : Employee(name, company, age)
+    {
+        Subject = subject;
+    }
+    void Work()
+    {
+        std::cout << Name << " is teaching " << Subject << ".\n";
+    }
 };
 
 int main()
 {
-    Employee employee1 = Employee("Kaiser", "TigerIT", 25);
-    Employee employee2 = Employee("Corey", "Google", 35);
+    Developer dev = Developer("Kaiser", "TigerIT", 25, "C++");
+    dev.IntroduceYourself(); // from Employee
+    dev.FixBug(); // from Developer
+    dev.AskForPromotion(); // from abstract class
 
-    employee1.AskForPromotion();
-    employee2.AskForPromotion();
+    Teacher tea = Teacher("Corey", "Google", 35, "Math");
+    tea.IntroduceYourself(); // from Employee
+    tea.PrepareLesson(); // from Teacher
+    tea.AskForPromotion(); // from abstract class
 
+    // The most common use of Polymorphism is when a parent class reference is used to refer to a child class object.
+    dev.Work();
+    tea.Work();
+
+    Employee *emp1 = &dev;
+    Employee *emp2 = &tea;
+
+    emp1 -> Work();
+    emp2 -> Work();
 }
