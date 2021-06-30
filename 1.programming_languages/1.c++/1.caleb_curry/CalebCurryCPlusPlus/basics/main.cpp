5bd55fd7abe9c15a1d10ab4c0b930fb36cadfa21
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-06-30 13:51:58 +0600

    updated!

--- 1.programming_languages/1.c++/1.caleb_curry/CalebCurryCPlusPlus/basics/main.cpp
+++ 1.programming_languages/1.c++/1.caleb_curry/CalebCurryCPlusPlus/basics/main.cpp
@@ -1125,3 +1125,60 @@ Intro to Arrays and Vectors
 =====================================================
 */
 
+
+
+/*
+=====================================================
+Working with Arrays
+=====================================================
+*/
+
+//#include <iostream>
+//#include <string>
+//
+//int main()
+//{
+//    //int guesses[7] = {10, 13, 54, 42, 12, 12, 13};
+//    //std::cout << guesses[3] << "\n";
+//    //guesses[3] = 300;
+//    //std::cout << guesses[3] << "\n";
+//
+//    //int guesses[20];
+//    //guesses[0] = 10;
+//    //std::cin >> guesses[0];
+//    //std::cout << guesses[0] << "\n";
+//
+//    //int guesses[] = {12, 43, 23, 43, 23};
+//    //int size = sizeof(guesses) / sizeof(guesses[0]); // sizeof(int) is also valid
+//    //std::cout << size << "\n";
+//    //for(int i = 0; i < size; i++)
+//    //{
+//    //    std::cout << guesses[i] << "\t";
+//    //}
+//    //std::cout << "\n";
+//
+//    int guesses[10] = {12, 43, 23, 43, 23};
+//    int num_elements = 5;
+//    int size = sizeof(guesses) / sizeof(guesses[0]);
+//    for(int i = 0; i < num_elements; i++)
+//    {
+//        std::cout << guesses[i] << "\t";
+//    }
+//    std::cout << "\n";
+//}
+
+
+
+/*
+=====================================================
+Passing Arrays to Functions
+=====================================================
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+}
