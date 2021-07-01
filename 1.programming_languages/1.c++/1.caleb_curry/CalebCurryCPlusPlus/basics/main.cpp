a6c1e26af3f442ee5ed0d379a1944a430b1452f4
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-02 02:14:34 +0600

    updated!

--- 1.programming_languages/1.c++/1.caleb_curry/CalebCurryCPlusPlus/basics/main.cpp
+++ 1.programming_languages/1.c++/1.caleb_curry/CalebCurryCPlusPlus/basics/main.cpp
@@ -1350,10 +1350,151 @@ Passing Vectors to Functions
 =====================================================
 */
 
+//#include <iostream>
+//#include <vector>
+//
+//// pass vector to a function
+////void print_vector(std::vector<int> data)
+////{
+////    for(int i = 0; i < data.size(); i++)
+////    {
+////        std::cout << data[i] << "\t";
+////    }
+////    std::cout << "\n";
+////}
+////
+////int main()
+////{
+////    std::vector<int> data = {1, 2, 3};
+////    print_vector(data);
+////}
+//
+//
+//// pass by reference
+////void print_vector(std::vector<int> data)
+////{
+////    data.push_back(12);
+////    for(int i = 0; i < data.size(); i++)
+////    {
+////        std::cout << data[i] << "\t";
+////    }
+////    std::cout << "\n";
+////}
+////
+////int main()
+////{
+////    std::vector<int> data = {1, 2, 3};
+////    print_vector(data);
+////    print_vector(data);
+////    print_vector(data);
+////}
+//
+//
+//// pass by reference
+//void print_vector(std::vector<int> &data)
+//{
+//    data.push_back(12);
+//    for(int i = 0; i < data.size(); i++)
+//    {
+//        std::cout << data[i] << "\t";
+//    }
+//    std::cout << "\n";
+//}
+//
+//int main()
+//{
+//    std::vector<int> data = {1, 2, 3};
+//    print_vector(data);
+//    print_vector(data);
+//    print_vector(data);
+//}
+
+
+
+/*
+=====================================================
+Refactor Guessing Game to Use Vectors
+=====================================================
+*/
+
+//#include <iostream>
+//#include <string>
+//#include <cstdlib>
+//#include <ctime>
+//#include <vector>
+//
+//void print_vector(std::vector<int> data)
+//{
+//    for(int i = 0; i < data.size(); i++)
+//    {
+//        std::cout << data[i] << "\t";
+//    }
+//    std::cout << "\n";
+//}
+//
+//void play_game()
+//{
+//    std::vector<int> guesses;
+//
+//    int random = rand() % 251;
+//    std::cout << "The number you have to guess: " << random << "\n";
+//    std::cout << "Guess a number: ";
+//    while(true)
+//    {
+//        int guess;
+//        std::cin >> guess;
+//        guesses.push_back(guess);
+//
+//        if(guess == random)
+//        {
+//            std::cout << "You win!\n";
+//            break;
+//        }
+//        else if(guess < random)
+//        {
+//            std::cout << "Too low\n";
+//        }
+//        else
+//        {
+//            std::cout << "Too high\n";
+//        }
+//    }
+//    print_vector(guesses);
+//}
+//
+//int main()
+//{
+//    srand(time(NULL));
+//    int choice;
+//    do
+//    {
+//        std::cout << "0. Quit\n" << "1. Play Game\n";
+//        std::cin>> choice;
+//        switch(choice)
+//        {
+//            case 0:
+//                std::cout << "Thanks for nothing!\n";
+//                return 0;
+//            case 1:
+//                play_game();
+//                break;
+//        }
+//    }while(choice !=  0);
+//}
+
+
+
+/*
+=====================================================
+STL Array
+=====================================================
+*/
+
 #include <iostream>
+#include <string>
 #include <vector>
 
 int main()
 {
-    std::vector<int> data = {1, 2, 3};
+
 }
