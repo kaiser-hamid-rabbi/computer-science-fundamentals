d31de57b729e6a4ec24c2f2c6ffe580caf0b5603
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-02 13:58:09 +0600

    updated!

--- 1.programming_languages/1.c++/1.caleb_curry/CalebCurryCPlusPlus/basics/main.cpp
+++ 1.programming_languages/1.c++/1.caleb_curry/CalebCurryCPlusPlus/basics/main.cpp
@@ -1539,5 +1539,64 @@ Refactor Guessing Game to Use Templatized Array
 #include <string>
 #include <vector>
 #include <array>
+#include <cstdlib>
+#include <ctime>
 
+void print_vector(std::vector<int> data)
+{
+    for(int i = 0; i < data.size(); i++)
+    {
+        std::cout << data[i] << "\t";
+    }
+    std::cout << "\n";
+}
 
+void play_game()
+{
+    std::vector<int> guesses;
+
+    int random = rand() % 251;
+    std::cout << "The number you have to guess: " << random << "\n";
+    std::cout << "Guess a number: ";
+    while(true)
+    {
+        int guess;
+        std::cin >> guess;
+        guesses.push_back(guess);
+
+        if(guess == random)
+        {
+            std::cout << "You win!\n";
+            break;
+        }
+        else if(guess < random)
+        {
+            std::cout << "Too low\n";
+        }
+        else
+        {
+            std::cout << "Too high\n";
+        }
+    }
+    print_vector(guesses);
+}
+
+int main()
+{
+    srand(time(NULL));
+    int choice;
+    do
+    {
+        std::cout << "0. Quit\n" << "1. Play Game\n";
+        std::cin>> choice;
+        switch(choice)
+        {
+            case 0:
+                std::cout << "Thanks for nothing!\n";
+                return 0;
+            case 1:
+                play_game();
+                break;
+        }
+    }while(choice !=  0);
+}
