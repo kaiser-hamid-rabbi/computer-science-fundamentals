bed4a9538105160e571f0eacb7e12a14c1cb9484
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-03 16:16:27 +0600

    updated!

--- 1.programming_languages/1.c++/1.caleb_curry/CalebCurry/main.cpp
+++ 1.programming_languages/1.c++/1.caleb_curry/CalebCurry/main.cpp
@@ -1761,6 +1761,113 @@ Saving High Scores to File
 =====================================================
  */
 
+// #include <iostream>
+// #include <string>
+// #include <cstdlib>
+// #include <ctime>
+// #include <vector>
+// #include <fstream>
+
+// void print_vector(std::vector<int> data)
+// {
+//    for(int i = 0; i < data.size(); i++)
+//    {
+//        std::cout << data[i] << "\t";
+//    }
+//    std::cout << "\n";
+// }
+
+// void play_game()
+// {
+//    std::vector<int> guesses;
+//    int count = 0;
+
+//    int random = rand() % 251;
+//    std::cout << "The number you have to guess: " << random << "\n";
+//    std::cout << "Guess a number: ";
+//    while(true)
+//    {
+//        int guess;
+//        std::cin >> guess;
+//        count++;
+//        guesses.push_back(guess);
+
+//        if(guess == random)
+//        {
+//            std::cout << "You win!\n";
+//            break;
+//        }
+//        else if(guess < random)
+//        {
+//            std::cout << "Too low\n";
+//        }
+//        else
+//        {
+//            std::cout << "Too high\n";
+//        }
+//    }
+//    std::ifstream input("best-score.txt");
+//    if(!input.is_open())
+//    {
+//        std::cout << "Unable to read file!\n";
+//        return;
+//    }
+//    int best_score;
+//    input >> best_score;
+//    std::ofstream output("best-score.txt");
+//    if(!output.is_open())
+//    {
+//        std::cout << "Unable to read file!\n";
+//        return;
+//    }
+//    if(count < best_score)
+//    {
+//        output << count;
+//    }
+//    else
+//    {
+//        output << best_score;
+//    }
+
+//    print_vector(guesses);
+// }
+
+// int main()
+// {
+//    srand(time(NULL));
+//    int choice;
+//    do
+//    {
+//        std::cout << "0. Quit\n" << "1. Play Game\n";
+//        std::cin>> choice;
+//        switch(choice)
+//        {
+//            case 0:
+//                std::cout << "Thanks for nothing!\n";
+//                return 0;
+//            case 1:
+//                play_game();
+//                break;
+//        }
+//    }while(choice !=  0);
+// }
+
+
+
+/* 
+=====================================================
+Functions and Constructors
+=====================================================
+ */
+
+
+
+/* 
+=====================================================
+Refactoring IO to Function Call and Testing
+=====================================================
+ */
+
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -1850,4 +1957,4 @@ int main()
                break;
        }
    }while(choice !=  0);
-} 
+}
