6405bbb95e0bfe8bc8e1f9320eee0861ee3848f7
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-04 12:03:19 +0600

    updated!

--- 1.programming_languages/1.c++/2.thenewboston/TheNewBoston/main.cpp
+++ 1.programming_languages/1.c++/2.thenewboston/TheNewBoston/main.cpp
@@ -22,12 +22,4 @@ int main() //main function
    std::cout << "Hello, World Rabbi!\n";
    std::cout << "Hello, World Rabbi!" << "\n";
    std::cout << "Hello, World Rabbi!" << std::endl;
-
-   int i_slices = 10;
-   double d_slices_1 = 20.;
-   double d_slices_2 = 20.0;
-   printf("Hello, World Rabbi!\n");
-   printf("%i\n", i_slices);
-   printf("%f\n", d_slices_1);
-   printf("%f\n", d_slices_1);
 }
