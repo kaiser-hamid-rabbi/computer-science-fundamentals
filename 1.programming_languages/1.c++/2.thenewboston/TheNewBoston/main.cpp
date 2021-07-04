d9bd9944fc0b4f1278e8d8828f1d6d45f48dbc97
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-04 13:31:03 +0600

    updated!

--- 1.programming_languages/1.c++/2.thenewboston/TheNewBoston/main.cpp
+++ 1.programming_languages/1.c++/2.thenewboston/TheNewBoston/main.cpp
@@ -24,6 +24,95 @@ More C++ Concepts
 =====================================================
 Installing CodeBlocks
 Simple C++ Program
+Printing Text
+=====================================================
+*/
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main() //main function
+// {
+//    cout << "Hello, World Rabbi!\n";
+//    cout << "Hello, World Rabbi!" << "\n";
+//    cout << "Hello, World Rabbi!" << endl;
+// }
+
+
+
+/*
+=====================================================
+Variables
+=====================================================
+*/
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//    int tuna = 6;
+//    cout << "tuna: " << tuna << endl;
+
+//    int a = 4;
+//    int b = 21;
+//    int sum = a + b;
+//    cout << "sum: " << sum << endl;
+// }
+
+
+
+/*
+=====================================================
+Basic Calculator
+=====================================================
+*/
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//    int a; // declaring variable
+//    int b;
+//    int sum;
+//    cout << "enter a number hoss! \n";
+//    cin >> a;
+//    cout << "enter another number \n";
+//    cin >> b;
+
+//    sum = a + b;
+//    cout << "the sum of those numbers is: " << sum << endl;
+// }
+
+
+
+/*
+=====================================================
+Variable Memory
+=====================================================
+*/
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//    int tuna; // declaration
+//    tuna = 54; // initialization
+//    tuna = 76; // override the initial value
+//    cout << "tuna: " << tuna << endl;
+// }
+
+
+
+/*
+=====================================================
+Basic Arithmetic
 =====================================================
 */
 
@@ -31,9 +120,10 @@ Simple C++ Program
 
 using namespace std;
 
-int main() //main function
+int main()
 {
-   cout << "Hello, World Rabbi!\n";
-   cout << "Hello, World Rabbi!" << "\n";
-   cout << "Hello, World Rabbi!" << endl;
+   int tuna; // declaration
+   tuna = 54; // initialization
+   tuna = 76; // override the initial value
+   cout << "tuna: " << tuna << endl;
 }
