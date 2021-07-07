03d90f4b19ac29b8d253e08ccfa4ba5cccbe6617
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-07 22:06:28 +0600

    a

--- 1.programming_languages/1.c++/1.caleb_curry/CalebCurry/basics/tempCodeRunnerFile.cpp
+++ 1.programming_languages/1.c++/1.caleb_curry/CalebCurry/basics/tempCodeRunnerFile.cpp
@@ -1,10 +1,10 @@
-    std::vector <std::string> names;
-    std::string input;
-    while(file >> input) // return file
-    {
-        names.push_back(input);
-    }
-    for(std::string name : names)
-    {
-        std::cout << name << "\n";
-    }
+std::vector<std::string> names;
+std::string input;
+while (file >> input) // return file
+{
+    names.push_back(input);
+}
+for (std::string name : names)
+{
+    std::cout << name << "\n";
+}
