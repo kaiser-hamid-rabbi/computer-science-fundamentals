18faa67d4abd883343a1cf3a9e49e856cebbc335
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-08 22:46:52 +0600

    a

--- 1.programming_languages/1.c++/2.thenewboston/TheNewBoston/src/main.cpp
+++ 1.programming_languages/1.c++/2.thenewboston/TheNewBoston/src/main.cpp
@@ -713,16 +713,49 @@ Default arguments
 =====================================================
 */
 
-#include <iostream>
+// // // one way
+// // #include <iostream>
 
-int volume(int l, int w, int h);
+// // int volume(int l = 1, int w = 1, int h = 1);
+
+// // int main()
+// // {
+// //     std::cout << volume(4, 5, 5) << "\n";
+// //     std::cout << volume() << "\n";     // 1 * 1 * 1
+// //     std::cout << volume(5) << "\n";    // 5 * 1 * 1
+// //     std::cout << volume(5, 6) << "\n"; // 5 * 6 * 1
+// // }
+
+// // int volume(int l, int w, int h)
+// // {
+// //     return l * w * h;
+// // }
+
+// // another way (recommended)
+// #include <iostream>
+
+// int volume(int l = 1, int w = 1, int h = 1)
+// {
+//     return l * w * h;
+// }
+
+// int main()
+// {
+//     std::cout << volume(4, 5, 5) << "\n";
+//     std::cout << volume() << "\n";     // 1 * 1 * 1
+//     std::cout << volume(5) << "\n";    // 5 * 1 * 1
+//     std::cout << volume(5, 6) << "\n"; // 5 * 6 * 1
+// }
+
+/*
+=====================================================
+Scope resolution operator
+=====================================================
+*/
+
+#include <iostream>
 
 int main()
 {
-    std::cout << volume(4, 5, 5) << "\n";
-}
-
-int volume(int l, int w, int h)
-{
-    return l * w * h;
+    std::cout << "hello...\n";
 }
--- 1.programming_languages/1.c++/2.thenewboston/TheNewBoston/src/tempCodeRunnerFile.cpp
+++ 1.programming_languages/1.c++/2.thenewboston/TheNewBoston/src/tempCodeRunnerFile.cpp
@@ -1,5 +1,15 @@
-rand(time(0));
-    for (int x = 0; x < 25; x++)
-    {
-        std::cout << 1 + (std::rand()) % 6 << "\n"; // random number between 1 to 6
-    }
+// another way (recommended)
+#include <iostream>
+
+int volume(int l = 1, int w = 1, int h = 1)
+{
+    return l * w * h;
+}
+
+int main()
+{
+    std::cout << volume(4, 5, 5) << "\n";
+    std::cout << volume() << "\n";     // 1 * 1 * 1
+    std::cout << volume(5) << "\n";    // 5 * 1 * 1
+    std::cout << volume(5, 6) << "\n"; // 5 * 6 * 1
+}
