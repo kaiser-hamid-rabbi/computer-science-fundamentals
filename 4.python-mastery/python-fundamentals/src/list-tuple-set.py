ab2be875fabe430b33735f964a66a59b7eeb901a
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-10 11:20:18 +0600

    a

--- 4.python-mastery/python-fundamentals/src/list-tuple-set.py
+++ 4.python-mastery/python-fundamentals/src/list-tuple-set.py
@@ -18,16 +18,16 @@ Set => no duplicates
 # --------------------- Lists ---------------------------------------
 # you can modify Lists(mutable). List is a mutable dynamic array.
 
-courses = ['History', 'Math', 'Physics', 'CompSci']
+# courses = ['History', 'Math', 'Physics', 'CompSci']
 
-print(courses)
-print(len(courses))
+# print(courses)
+# print(len(courses))
 
-print(courses[0])
-print(courses[3])
-print(courses[2])
-print(courses[-1])
-print(courses[-2])
+# print(courses[0])
+# print(courses[3])
+# print(courses[2])
+# print(courses[-1])
+# print(courses[-2])
 
 
 # ---------------------- Slicing -------------------------
