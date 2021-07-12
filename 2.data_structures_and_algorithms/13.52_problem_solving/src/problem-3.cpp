b517e4592c983453a35cdda832d2d7d756222b9a
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-12 15:21:01 +0600

    a

--- 2.data_structures_and_algorithms/13.52_problem_solving/src/problem-3.cpp
+++ 2.data_structures_and_algorithms/13.52_problem_solving/src/problem-3.cpp
@@ -2,12 +2,15 @@
 
 int main()
 {
+    int count = 0;
     for (int i = 1000; i >= 1; i--)
     {
+        count++;
         std::cout << i << "\t";
-        if (i % 5 == 0)
+        if (count % 5 == 0)
         {
             std::cout << "\n";
         }
     }
+    std::cout << "\n";
 }
