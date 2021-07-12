dc40497476df46a1799972a39c18fd4ea55a201a
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-12 14:18:55 +0600

    a

--- 2.data_structures_and_algorithms/13.52_problem_solving/src/problem-1.cpp
+++ 2.data_structures_and_algorithms/13.52_problem_solving/src/problem-1.cpp
@@ -2,5 +2,18 @@
 
 int main()
 {
-    std::cout << "hello...\n";
+    while (true)
+    {
+        int number;
+        std::cout << "Enter a number: ";
+        std::cin >> number;
+        if (number % 2 == 0)
+        {
+            std::cout << "even\n";
+        }
+        else
+        {
+            std::cout << "odd\n";
+        }
+    }
 }
