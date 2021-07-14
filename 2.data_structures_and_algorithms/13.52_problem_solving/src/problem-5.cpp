237b3b0cb6ce5efd81edbe8ef80da02293beb95f
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-14 10:25:50 +0600

    a

--- 2.data_structures_and_algorithms/13.52_problem_solving/src/problem-5.cpp
+++ 2.data_structures_and_algorithms/13.52_problem_solving/src/problem-5.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "hello...\n";
+    int T, i, N, row, column;
+    std::cin >> T;
+    for (i = 1; i <= T; i++)
+    {
+        std::cin >> N;
+        for (row = 1; row <= N; row++)
+        {
+            for (column = 1; column <= N; column++)
+            {
+                std::cout << "*";
+            }
+            std::cout << "\n";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
 }
