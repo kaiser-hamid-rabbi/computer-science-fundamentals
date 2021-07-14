6dc64c7f67cbf1d6070e4144da75034876e2ba04
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-14 09:29:11 +0600

    a

--- 2.data_structures_and_algorithms/13.52_problem_solving/src/problem-4.cpp
+++ 2.data_structures_and_algorithms/13.52_problem_solving/src/problem-4.cpp
@@ -2,5 +2,19 @@
 
 int main()
 {
-    std::cout << "hello...\n";
+    int T, i, N, j;
+    std::cin >> T;
+    for (i = 1; i <= T; i++)
+    {
+        std::cin >> N;
+        std::cout << "Case " << i << ": ";
+        for (j = 1; j <= N; j++)
+        {
+            if (N % j == 0)
+            {
+                std::cout << j << " ";
+            }
+        }
+        std::cout << "\n";
+    }
 }
