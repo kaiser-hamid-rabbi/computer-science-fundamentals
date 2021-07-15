b999332e1aada2e6549682077dcc59cbda8e5ec1
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-16 01:39:17 +0600

    a

--- 1.programming_languages/2.python/5.oop/5.1.techwithtim/TechWithTim/oop.py
+++ 1.programming_languages/2.python/5.oop/5.1.techwithtim/TechWithTim/oop.py
@@ -96,3 +96,24 @@ class Course:
         self.name = name
         self.max_students = max_students
         self.students = []
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
+    def get_average_grade(self):
+        value = 0
+        for student in self.students:
+            value += student.get_grade()
+        return value / len(self.students)
+
+s1 = Student("Tim", 19, 95)
+s2 = Student("Bill", 20, 75)
+s3 = Student("Jill", 18, 65)
+
+course = Course("Science", 2)
+course.add_student(s1)
+course.add_student(s2)
+print(course.students[0].name)
+print(course.students[1].name)
+print(course.get_average_grade())
