8a2d2cc5f7a6058687ee3c462295f5ff57db07aa
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-16 01:46:03 +0600

    a

--- 1.programming_languages/2.python/5.oop/5.1.techwithtim/TechWithTim/oop.py
+++ 1.programming_languages/2.python/5.oop/5.1.techwithtim/TechWithTim/oop.py
@@ -80,40 +80,51 @@ Encapsulation
 
 """
 ===========================================
-Encapsulation
+Data passing between two classes
 ===========================================
 """
-class Student:
-    def __init__(self, name, age, grade):
-        self.name = name
-        self.age = age
-        self.grade = grade # 0 - 100
-    def get_grade(self):
-        return self.grade
+# class Student:
+#     def __init__(self, name, age, grade):
+#         self.name = name
+#         self.age = age
+#         self.grade = grade # 0 - 100
+#     def get_grade(self):
+#         return self.grade
 
-class Course:
-    def __init__(self, name, max_students):
-        self.name = name
-        self.max_students = max_students
-        self.students = []
-    def add_student(self, student):
-        if len(self.students) < self.max_students:
-            self.students.append(student)
-            return True
-        return False
-    def get_average_grade(self):
-        value = 0
-        for student in self.students:
-            value += student.get_grade()
-        return value / len(self.students)
+# class Course:
+#     def __init__(self, name, max_students):
+#         self.name = name
+#         self.max_students = max_students
+#         self.students = []
+#     def add_student(self, student):
+#         if len(self.students) < self.max_students:
+#             self.students.append(student)
+#             return True
+#         return False
+#     def get_average_grade(self):
+#         value = 0
+#         for student in self.students:
+#             value += student.get_grade()
+#         return value / len(self.students)
 
-s1 = Student("Tim", 19, 95)
-s2 = Student("Bill", 20, 75)
-s3 = Student("Jill", 18, 65)
+# s1 = Student("Tim", 19, 95)
+# s2 = Student("Bill", 20, 75)
+# s3 = Student("Jill", 18, 65)
 
-course = Course("Science", 2)
-course.add_student(s1)
-course.add_student(s2)
-print(course.students[0].name)
-print(course.students[1].name)
-print(course.get_average_grade())
+# course = Course("Science", 2)
+# course.add_student(s1)
+# course.add_student(s2)
+# print(course.students[0].name)
+# print(course.students[1].name)
+# print(course.get_average_grade())
+
+# course.add_student(s3)
+# print(course.add_student(s3))
+# # print(course.students[2].name) # you will get an error
+# print(course.get_average_grade())
+
+"""
+===========================================
+Inheritence
+===========================================
+"""
