a7e7af527d58457f45cc36166cc511a0c48e6aa0
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-15 23:28:00 +0600

    a

--- 1.programming_languages/2.python/5.oop/5.1.techwithtim/TechWithTim/oop.py
+++ 1.programming_languages/2.python/5.oop/5.1.techwithtim/TechWithTim/oop.py
@@ -2,7 +2,7 @@
 
 """
 ===========================================
-Part - 1
+Class and Instance
 ===========================================
 """ 
 # class Dog:
@@ -13,7 +13,7 @@ Part - 1
 #     def add_one(self, x):
 #         return x + 1
 
-# d = Dog()
+# d = Dog() # instance of Dog class
 # print(type(d))
 # d.bark()
 # print(d.meow())
@@ -21,20 +21,78 @@ Part - 1
 
 """
 ===========================================
-Part - 2
+Constructor, method
 ===========================================
 """
-class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age =age
-    def meow(self):
-        return "meow"
-    def bark(self):
-        print("bark")
-    def add_one(self, x):
-        return x + 1
+# class Dog:
+#     def __init__(self, name, age):
+#         # instance variables
+#         self.name = name
+#         self.age =age
+#     def meow(self):
+#         return "meow"
+#     def bark(self):
+#         print("bark")
+#     def add_one(self, x):
+#         return x + 1
 
-d = Dog("Golden Retriever", 8)
-print(d.name)
-print(d.age)
+# d1 = Dog("Golden Retriever", 8)
+# print(d1.name)
+# print(d1.age)
+# d2 = Dog("Samoyed", 10)
+# print(d2.name)
+# print(d2.age)
+
+"""
+===========================================
+Encapsulation
+===========================================
+"""
+# class Dog:
+#     def __init__(self, name, age):
+#         # instance variables
+#         self._name = name
+#         self._age =age
+#     def get_name(self): # getter
+#         return self._name
+#     def set_name(self, name): # setter
+#         self._name = name
+#     def get_age(self):
+#         return self._age
+#     def set_age(self, age):
+#         self._age = age
+
+# d1 = Dog("Golden Retriever", 8)
+# print(d1.get_name())
+# print(d1.get_age())
+# d1.set_name("Ricky")
+# d1.set_age(10)
+# print(d1.get_name())
+# print(d1.get_age())
+
+# d2 = Dog("Samoyed", 12)
+# print(d2.get_name())
+# print(d2.get_age())
+# d2.set_name("Monty")
+# d2.set_age(14)
+# print(d2.get_name())
+# print(d2.get_age())
+
+"""
+===========================================
+Encapsulation
+===========================================
+"""
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade # 0 - 100
+    def get_grade(self):
+        return self.grade
+
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
