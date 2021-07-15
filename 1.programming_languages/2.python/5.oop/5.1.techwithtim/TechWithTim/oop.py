5242d2cd1b35c265b65847e39c14b120ab066fe0
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-15 22:23:08 +0600

    a

--- 1.programming_languages/2.python/5.oop/5.1.techwithtim/TechWithTim/oop.py
+++ 1.programming_languages/2.python/5.oop/5.1.techwithtim/TechWithTim/oop.py
@@ -1,1 +1,41 @@
-print("hello...")
+# Object Oriented Programming in Python
+
+"""
+Part - 1
+"""
+# class Dog:
+#     def meow(self):
+#         return "meow"
+#     def bark(self):
+#         print("bark")
+#     def add_one(self, x):
+#         return x + 1
+
+# d = Dog()
+# print(type(d))
+# d.bark()
+# print(d.meow())
+# print(d.add_one(5))
+
+"""
+Part - 2
+"""
+class Dog:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age =age
+
+    def meow(self):
+        return "meow"
+
+    def bark(self):
+        print("bark")
+
+    def add_one(self, x):
+        return x + 1
+
+
+d = Dog("Golden Retriever", 8)
+print(d.name)
+print(d.age)
