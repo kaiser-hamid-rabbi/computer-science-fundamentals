49639036e0cdec14b9369f8291b1532aac5b91f7
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-16 19:27:06 +0600

    a

--- 1.programming_languages/2.python/5.oop/5.2.pythonengineer/oop1.py
+++ 1.programming_languages/2.python/5.oop/5.2.pythonengineer/oop1.py
@@ -5,6 +5,6 @@ Create Classes
 Class vs Instance
 ============================
 """
-# position, 
-se1 = []
-se2 = []
+# position, name, age, level, salary
+se1 = ["Software Engineer", "Max", 20, "Junior", 5000]
+se2 = ["Software Engineer", "Lisa", 25, "Senior", 7000]
