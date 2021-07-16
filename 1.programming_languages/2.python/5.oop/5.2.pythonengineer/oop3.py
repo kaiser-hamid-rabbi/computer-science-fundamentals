e3d2ff3ac99e7bccc4fcb8402d59e4b11aa1fc87
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-16 20:38:42 +0600

    a

--- 1.programming_languages/2.python/5.oop/5.2.pythonengineer/oop3.py
+++ 1.programming_languages/2.python/5.oop/5.2.pythonengineer/oop3.py
@@ -6,25 +6,37 @@ Inheritance
 """
 # inherits, extend, override
 class Employee:
-    def __init__(self, name, age):
+    def __init__(self, name, age, salary):
         self.name = name
         self.age = age
+        self.salary = salary
     def work(self):
         print(f"{self.name} is working...")
 
 class SoftwareEngineer(Employee):
-    def __init__(self, name, age, level, salary):
-        super().__init__(name, age)
+    def __init__(self, name, age, salary, level):
+        super().__init__(name, age, salary)
         self.level = level
-        self.salary = salary
+    def debug(self):
+        print(f"{self.name} is debugging...")
+        
 
 class Designer(Employee):
-    pass
+    def draw(self):
+        print(f"{self.name} is drawing...")
 
-se = SoftwareEngineer("Max", 25)
-print(se.name, se.age)
+# se = SoftwareEngineer("Max", 25, 6000)
+# print(se.name, se.age)
+# se.work()
+# d = Designer("Philip", 27, 7000)
+# print(d.name, d.age)
+# d.work()
+
+se = SoftwareEngineer("Max", 25, 6000, "Junior")
 se.work()
-
-d = Designer("Philip", 27)
-print(d.name, d.age)
+print(se.level)
+se.debug()
+se.draw() # you will get an error because draw() is attached to Designer class only, not attached with SoftwareEngineer class
+d = Designer("Philip", 27, 7000)
 d.work()
+d.draw()
