9d0dea172185e7b4eefb8ef275733e5326013b9c
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-17 02:14:50 +0600

    a

--- 1.programming_languages/2.python/5.oop/5.2.pythonengineer/oop3.py
+++ 1.programming_languages/2.python/5.oop/5.2.pythonengineer/oop3.py
@@ -17,11 +17,15 @@ class SoftwareEngineer(Employee):
     def __init__(self, name, age, salary, level):
         super().__init__(name, age, salary)
         self.level = level
+    def work(self):
+        print(f"{self.name} is coding...")
     def debug(self):
         print(f"{self.name} is debugging...")
         
 
 class Designer(Employee):
+    def work(self):
+        print(f"{self.name} is designing...")
     def draw(self):
         print(f"{self.name} is drawing...")
 
@@ -32,11 +36,20 @@ class Designer(Employee):
 # print(d.name, d.age)
 # d.work()
 
-se = SoftwareEngineer("Max", 25, 6000, "Junior")
-se.work()
-print(se.level)
-se.debug()
-se.draw() # you will get an error because draw() is attached to Designer class only, not attached with SoftwareEngineer class
-d = Designer("Philip", 27, 7000)
-d.work()
-d.draw()
+# se = SoftwareEngineer("Max", 25, 6000, "Junior")
+# se.work()
+# se.debug()
+# print(se.level)
+# # se.draw() # you will get an error because draw() is attached to Designer class only, not attached with SoftwareEngineer class
+# d = Designer("Philip", 27, 7000)
+# d.work()
+# d.draw()
+
+# Polymorphism
+employees = [SoftwareEngineer("Max", 20, 5000, "Junior"),
+            SoftwareEngineer("Lisa", 30, 9000, "Senior"),
+            Designer("Philip", 27, 7000)]
+def motivate_employees(employees):
+    for employee in employees:
+        employee.work()
+motivate_employees(employees)
