85cd8fe32e811a599cb8e20b4da868c5b49d1780
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-17 12:36:43 +0600

    a

--- 1.programming_languages/2.python/5.oop/5.5.tamimshahriarsubeen/oop.py
+++ 1.programming_languages/2.python/5.oop/5.5.tamimshahriarsubeen/oop.py
@@ -1,9 +1,67 @@
+"""
+======================
+Class
+======================
+"""
+# # Way - 1
+# class Car:
+#     name = "premio" # class attribute
+#     color = "white" # class attribute
+#     def start():
+#         print("starting the engine...")
+
+# print("name of the car: ", Car.name)
+# print("color of the car: ", Car.color)
+# Car.start()
+
+# # Way - 2
+# class Car:
+#     name = ""
+#     color = ""
+#     def start():
+#         print("starting the engine...")
+
+# Car.name = "axio"
+# Car.color = "black"
+# print("name of the car: ", Car.name)
+# print("color of the car: ", Car.color)
+# Car.start()
+# print(dir(Car)) # 'name', 'color', 'start'
+
+"""
+======================
+Object
+======================
+"""
+# class Car:
+#     name = ""
+#     color = ""
+#     def start(self):
+#         print("starting the engine...")
+
+# my_car = Car()
+# my_car.name = "allion"
+# my_car.color = "blue"
+# print(my_car.name)
+# print(my_car.color)
+# my_car.start()
+
+"""
+======================
+Constructor
+======================
+"""
 class Car:
-    name = "premio"
-    color = "white"
-    def start():
+    # constructor
+    def __init__(self, name, color):
+        self.name = name # instance attribute
+        self.color = color # instance attribute
+    def start(self):
+        print("name: ", self.name)
+        print("color: ", self.color)
         print("starting the engine...")
 
-print("name of the car: ", Car.name)
-print("color of the car: ", Car.color)
-Car.start()
+my_car = Car("corolla", "white")
+print(my_car. name)
+print(my_car.color)
+my_car.start()
