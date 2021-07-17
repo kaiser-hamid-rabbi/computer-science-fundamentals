b679db5a6ac30fb2ae40bdacc5dafcf61f98464d
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-17 14:23:11 +0600

    a

--- 1.programming_languages/2.python/5.oop/5.5.tamimshahriarsubeen/oop-3.py
+++ 1.programming_languages/2.python/5.oop/5.5.tamimshahriarsubeen/oop-3.py
@@ -16,11 +16,31 @@ import turtle
 
 class WeirdTurtle(turtle.Turtle):
     """ WeirdTurtle is a class for new type of turtle """
-    pass
+    def forward(self, pixel):
+        super().backward(pixel)
+    def backward(self, pixel):
+        super().forward(pixel)
+    def left(self, angle):
+        super().right(angle)
+    def right(self, angle):
+        print(f"I won't turn right, because I am Weird!")
 
 if __name__ == "__main__":
     shelly = WeirdTurtle()
-    print(type(shelly))
-    shelly.left(90) # angle
-    shelly.forward(200) # pixel
+    shelly.left(30)
+    shelly.forward(200)
+    shelly.left(45)
+    shelly.backward(100)
+    shelly.right(90)
+    shelly.forward(10)
+
+    speedy = turtle.Turtle()
+    speedy.shape("turtle")
+    speedy.left(30)
+    speedy.forward(200)
+    speedy.left(45)
+    speedy.backward(100)
+    speedy.right(90)
+    speedy.forward(10)
+
     turtle.done()
