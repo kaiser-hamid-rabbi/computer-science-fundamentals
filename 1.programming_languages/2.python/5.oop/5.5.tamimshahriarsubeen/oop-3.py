30bd1ab52c2db1c78aff794783517aa21c004f43
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-17 14:05:59 +0600

    a

---
+++ 1.programming_languages/2.python/5.oop/5.5.tamimshahriarsubeen/oop-3.py
@@ -0,0 +1,26 @@
+# import turtle
+
+# class WeirdTurtle(turtle.Turtle):
+#     """ WeirdTurtle is a class for new type of turtle """
+#     pass
+
+# if __name__ == "__main__":
+#     shelly = WeirdTurtle()
+#     print(type(shelly))
+#     shelly.left(90) # angle
+#     shelly.forward(200) # pixel
+#     turtle.done()
+
+# Continue - 1
+import turtle
+
+class WeirdTurtle(turtle.Turtle):
+    """ WeirdTurtle is a class for new type of turtle """
+    pass
+
+if __name__ == "__main__":
+    shelly = WeirdTurtle()
+    print(type(shelly))
+    shelly.left(90) # angle
+    shelly.forward(200) # pixel
+    turtle.done()
