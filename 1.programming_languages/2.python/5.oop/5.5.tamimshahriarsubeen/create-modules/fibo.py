791d59285b023cfda79ce773d87f013e84c56393
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-18 01:03:46 +0600

    a

--- 1.programming_languages/2.python/5.oop/5.5.tamimshahriarsubeen/create-modules/fibo.py
+++ 1.programming_languages/2.python/5.oop/5.5.tamimshahriarsubeen/create-modules/fibo.py
@@ -1,9 +1,9 @@
 """
 =====================
-Fibonacchi Number
+Fibonacci Number
 =====================
 """
-# # Print first 10 fibonacchi number
+# # Print first 10 fibonacci number
 # def find_fib(n):
 #     if n <= 2:
 #         return 1
@@ -18,32 +18,30 @@ Fibonacchi Number
 # for x in range(1, 11):
 #     print(find_fib(x))
 
-# Fibonacchi number using list
+# Fibonacci number using list
 def find_fib(n):
-    if n <= 2:
-        return 1
-    fib_x, fib_next = 1, 1
-    i = 3
-    while i <= n:
-        i += 1
-        fib_x, fib_next = fib_next, fib_x + fib_next
-    return fib_next
-
+        if n <= 2:
+            return 1
+        fib_x, fib_next = 1, 1
+        i = 3
+        while i <= n:
+            i += 1
+            fib_x, fib_next = fib_next, fib_x + fib_next
+        return fib_next
 def list_fib(n):
-    fib_list = [1, 1]
-    if n <= 2:
-        return fib_list[:n]
-    fib_x, fib_next = 1, 1
-    i = 3
-    while i <= n:
-        i += 1
-        fib_x, fib_next = fib_next, fib_x + fib_next
-        fib_list.append(fib_next)
-    return fib_list
-
-for x in range(1, 11):
-    print(find_fib(x))
-
-print(list_fib(1))
-print(list_fib(2))
-print(list_fib(10))
+        fib_list = [1, 1]
+        if n <= 2:
+            return fib_list[:n]
+        fib_x, fib_next = 1, 1
+        i = 3
+        while i <= n:
+            i += 1
+            fib_x, fib_next = fib_next, fib_x + fib_next
+            fib_list.append(fib_next)
+        return fib_list
+if __name__ == "__main__":
+    for x in range(1, 11):
+        print(find_fib(x))
+    print(list_fib(1))
+    print(list_fib(2))
+    print(list_fib(10))
---
+++ 1.programming_languages/2.python/5.oop/5.5.tamimshahriarsubeen/create-modules/program.py
@@ -0,0 +1,11 @@
+import fibo
+
+n = fibo.find_fib(15)
+print(f"15th fibonacci number is {n}")
+print(fibo.__name__)
+
+
+# import trial
+
+# print("Hello, I am inside program.py!")
+# print(trial.__name__)
