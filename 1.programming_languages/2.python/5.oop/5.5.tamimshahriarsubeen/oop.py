0861bb39dc9c6c68d286519e84439a5d1cc4dd43
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-17 12:20:24 +0600

    a

--- 1.programming_languages/2.python/5.oop/5.5.tamimshahriarsubeen/oop.py
+++ 1.programming_languages/2.python/5.oop/5.5.tamimshahriarsubeen/oop.py
@@ -1,1 +1,9 @@
-print("hello...")
+class Car:
+    name = "premio"
+    color = "white"
+    def start():
+        print("starting the engine...")
+
+print("name of the car: ", Car.name)
+print("color of the car: ", Car.color)
+Car.start()
