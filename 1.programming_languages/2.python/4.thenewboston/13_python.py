57382baca938ab2ac0e08192316cd07fe0f6c5e0
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-20 21:51:36 +0600

    a

--- 1.programming_languages/2.python/4.thenewboston/13_python.py
+++ 1.programming_languages/2.python/4.thenewboston/13_python.py
@@ -2,7 +2,7 @@ def allowed_dating_age(my_age):
     girls_age = (my_age/2)+7
     return girls_age
 
-buckys_limit = allowed_dating_age(22)
+buckys_limit = allowed_dating_age(27)
 creppy_jow_limit = allowed_dating_age(49)
 print("Bucky can date girls", buckys_limit,"or older")
 print("Bucky can date girls", creppy_jow_limit,"or older")
