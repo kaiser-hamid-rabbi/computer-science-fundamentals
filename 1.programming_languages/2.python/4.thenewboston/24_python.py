add64c2b9ec68fd7aaa7e4436bf32d765ef9245f
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-21 01:04:00 +0600

    a

--- 1.programming_languages/2.python/4.thenewboston/24_python.py
+++ 1.programming_languages/2.python/4.thenewboston/24_python.py
@@ -1,6 +1,6 @@
 from urllib import request
 
-goog_url = http://real-chart.finance.yahoo.com/table.csv?s=GOOG&d=2&e=8&f=2015&g=d&a=2&b=27&c=2014&ignore=.csv
+goog_url = "http://real-chart.finance.yahoo.com/table.csv?s=GOOG&d=2&e=8&f=2015&g=d&a=2&b=27&c=2014&ignore=.csv"
 
 def download_stock_data(csv_url):
     response = request.urlopen(csv_url)
