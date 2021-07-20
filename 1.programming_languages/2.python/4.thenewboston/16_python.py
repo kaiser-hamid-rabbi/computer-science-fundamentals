bf54271c6f908c5b2ceea543c3950cfe5ff52fb2
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-20 22:03:18 +0600

    a

--- 1.programming_languages/2.python/4.thenewboston/16_python.py
+++ 1.programming_languages/2.python/4.thenewboston/16_python.py
@@ -4,4 +4,4 @@ def dumb_sentence(name='Bucky', action='ate', item='tuna'):
 dumb_sentence()
 dumb_sentence("Sally", "farts", "gently")
 dumb_sentence(item='awesome')
-# dume_sentence(item='awesome', action='is')
+dumb_sentence(item='awesome', action='is')
