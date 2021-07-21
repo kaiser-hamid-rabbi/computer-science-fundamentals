e09b2412167019898bcc28f6136209ed803f3bd3
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-21 20:43:21 +0600

    a

--- 1.programming_languages/2.python/4.thenewboston/25_python.py
+++ 1.programming_languages/2.python/4.thenewboston/25_python.py
@@ -4,10 +4,16 @@ from bs4 import BeautifulSoup
 
 def trade_spider(max_pages):
   page = 1
-  while page < max_pages:
+  while page <= max_pages:
     url = 'https://buckysroom.org/trade/search.php?page=' + str(page)
     source_code = requests.get(url)
-    # plain_text = 
-    print("kaiser")
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text)
+    for link in soup.findall('a', {'class': 'item-name'}):
+          href = 'https://buckysroom.org' + link.get('href')
+          title = link.string
+          print(href)
+          print(title)
+    page += 1
   
-trade_spider(10)
+trade_spider(1)
