11ccf6a3536b56eebc9014cb0d83b53e57a62a11
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-23 20:38:42 +0600

    a

--- 1.programming_languages/1.python/4.thenewboston/40_python.py
+++ 1.programming_languages/1.python/4.thenewboston/40_python.py
@@ -1,3 +1,7 @@
-# lambda is a small function that has no name!
+# lambda is a small function that has no name! it's one time use small functions
 answer = lambda x: x ** 5
 print(answer(5))
+
+# with multiple parameters
+answer = lambda x, y: (x * 5) + (y * 10)
+print(answer(5, 7))
