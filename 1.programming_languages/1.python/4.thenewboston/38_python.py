700c0f57e300097466d0b20f2ce86b68d2912479
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-23 20:26:44 +0600

    a

--- 1.programming_languages/1.python/4.thenewboston/38_python.py
+++ 1.programming_languages/1.python/4.thenewboston/38_python.py
@@ -6,6 +6,9 @@
 # drop_first_last([65, 76, 98, 54, 21])
 # drop_first_last([65, 76, 98, 54, 21, 54, 65, 99, 88, 78])
 
-
-item = ['December 23, 2015', 'Bread Gloves', 8.51]
-print(item[0])
+# way 1 - not recommended
+# item = ['December 23, 2015', 'Bread Gloves', 8.51]
+# print(item[0])
+# way 2 - unpack list - recommended
+date, name, price = ['December 23, 2015', 'Bread Gloves', 8.51]
+print(name)
