282af77151223e3d19aea9e8d300fa2ce083f71d
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-23 15:32:01 +0600

    a

--- 1.programming_languages/2.python/4.thenewboston/35_python.py
+++ 1.programming_languages/2.python/4.thenewboston/35_python.py
@@ -6,13 +6,14 @@ import operator
 def start(url):
     word_list = []
     source_code = requests.get(url).text
-    soup = BeautifulSoup(source_code)
+    soup = BeautifulSoup(source_code, features="html.parser")
     for post_text in soup.findAll('a', {'class': 'index_singleListingTitles'}):
         content = post_text.string
         words = content.lower().split()
         for each_word in words:
             print(each_word)
             word_list.append(each_word)
+    print(word_list)
 
 
 start('https://kaiser-hamid-rabbi.github.io/my-story.html')
