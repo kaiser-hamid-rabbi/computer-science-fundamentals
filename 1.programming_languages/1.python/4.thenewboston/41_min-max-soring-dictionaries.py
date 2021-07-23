f067232c6ff010d8ee5414fdcd1feb2d24c5f256
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-24 01:07:01 +0600

    a

--- 1.programming_languages/1.python/4.thenewboston/41_min-max-soring-dictionaries.py
+++ 1.programming_languages/1.python/4.thenewboston/41_min-max-soring-dictionaries.py
@@ -1,1 +1,13 @@
-print('hello...')
+stocks = {
+    'GOOG' : 520.54,
+    'FB' : 76.45,
+    'YAHOO' : 39.28,
+    'AMAZON' : 306.21,
+    'APPLE' : 99.76
+}
+
+# zip(stocks.values(), stocks.keys())
+print(min(zip(stocks.values(), stocks.keys())))
+print(max(zip(stocks.values(), stocks.keys())))
+print(sorted(zip(stocks.values(), stocks.keys())))
+print(sorted(zip(stocks.keys(), stocks.values())))
