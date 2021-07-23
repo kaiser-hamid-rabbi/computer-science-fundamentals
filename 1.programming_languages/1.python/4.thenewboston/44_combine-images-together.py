31abeab852ad96565977d1f54b1a448ab2f30f0e
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-24 02:02:46 +0600

    a

--- 1.programming_languages/1.python/4.thenewboston/44_combine-images-together.py
+++ 1.programming_languages/1.python/4.thenewboston/44_combine-images-together.py
@@ -1,2 +1,12 @@
 from PIL import Image
 
+doggo = Image.open("doggo.jpg")
+ziraf = Image.open("ziraf.jpg")
+
+print(doggo.size)
+print(ziraf.size)
+
+area = (300, 300)
+doggo.paste(ziraf, area)
+
+doggo.show()
