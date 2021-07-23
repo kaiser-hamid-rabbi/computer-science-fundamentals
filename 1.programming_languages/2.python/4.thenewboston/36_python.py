0e131a110958cd01239978fea0f8991cf5a955c1
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-23 15:43:27 +0600

    a

--- 1.programming_languages/2.python/4.thenewboston/36_python.py
+++ 1.programming_languages/2.python/4.thenewboston/36_python.py
@@ -6,7 +6,7 @@ import operator
 def start(url):
     word_list = []
     source_code = requests.get(url).text
-    soup = BeautifulSoup(source_code)
+    soup = BeautifulSoup(source_code, features="html.parser")
     for post_text in soup.findAll('a', {'class': 'index_singleListingTitles'}):
         content = post_text.string
         words = content.lower().split()
@@ -19,11 +19,12 @@ def clean_up_list(word_list):
     clean_word_list = []
     for word in word_list:
         symbols = "!@#$%^&*()_+{}:\"<>?,./;'[]-='"
-        for i in range(0, len(symbols):
+        for i in range(0, len(symbols)):
             word = word.replace(symbols[i], "")
         if len(word) > 0:
             print(word)
             clean_word_list.append(word)
+    print(clean_word_list)
 
 
 start('https://buckysroom.org/tops.php?type=text&period=this-month')
