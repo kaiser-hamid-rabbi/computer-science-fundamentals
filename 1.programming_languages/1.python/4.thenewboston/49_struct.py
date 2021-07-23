b62ef5125f0c7ce42c3dd2ac697ec2466ae3bfb3
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-24 02:45:12 +0600

    a

--- 1.programming_languages/1.python/4.thenewboston/49_struct.py
+++ 1.programming_languages/1.python/4.thenewboston/49_struct.py
@@ -1,1 +1,15 @@
-print('hello...')
+# struct convert any type of data to bytes format
+from struct import *
+
+# Store as bytes data
+packed_data = pack('iif', 6, 19, 4.73)
+print(packed_data)
+
+print(calcsize('i'))
+print(calcsize('f'))
+print(calcsize('iif'))
+
+# To get bytes data back to normal
+# original_data = unpack('iif', b'\x06\x00\x00\x00\x13\x00\x00\x00)\\\x97@')
+original_data = unpack('iif', packed_data)
+print(original_data)
