a4b82f79894e34c9f3d55e4f9d93725841d9465d
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-23 20:28:22 +0600

    a

--- 1.programming_languages/1.python/4.thenewboston/38_python.py
+++ 1.programming_languages/1.python/4.thenewboston/38_python.py
@@ -1,14 +1,17 @@
-# def drop_first_last(grades):
-#     first, *middle, last = grades
-#     avg = sum(middle) / len(middle)
-#     print(avg)
-
-# drop_first_last([65, 76, 98, 54, 21])
-# drop_first_last([65, 76, 98, 54, 21, 54, 65, 99, 88, 78])
-
-# way 1 - not recommended
+# # way 1 - not recommended
 # item = ['December 23, 2015', 'Bread Gloves', 8.51]
 # print(item[0])
-# way 2 - unpack list - recommended
-date, name, price = ['December 23, 2015', 'Bread Gloves', 8.51]
-print(name)
+# # way 2 - unpack list - recommended
+# date, name, price = ['December 23, 2015', 'Bread Gloves', 8.51]
+# print(name)
+
+
+def drop_first_last(grades):
+    first, *middle, last = grades
+    avg = sum(middle) / len(middle)
+    print(avg)
+
+drop_first_last([65, 76, 98, 54, 21])
+drop_first_last([65, 76, 98, 54, 21, 54, 65, 99, 88, 78])
+
+
