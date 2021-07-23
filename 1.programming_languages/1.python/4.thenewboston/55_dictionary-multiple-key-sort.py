51849b83a2641116eaa5d0d2dde910e0bbaf7882
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-24 03:20:20 +0600

    a

---
+++ 1.programming_languages/1.python/4.thenewboston/55_dictionary-multiple-key-sort.py
@@ -0,0 +1,21 @@
+from operator import itemgetter
+
+users = [
+    {'fname' : 'Bucky', 'lname' : 'Roberts'},
+    {'fname' : 'Tom', 'lname' : 'Roberts'},
+    {'fname' : 'Bernie', 'lname' : 'Zunks'},
+    {'fname' : 'Sally', 'lname' : 'Jones'},
+    {'fname' : 'Amanda', 'lname' : 'Roberts'},
+    {'fname' : 'Tom', 'lname' : 'Williams'},
+    {'fname' : 'Dean', 'lname' : 'Hayes'},
+    {'fname' : 'Bernie', 'lname' : 'Barbie'},
+    {'fname' : 'Tom', 'lname' : 'Jones'}
+]
+
+for x in sorted(users, key=itemgetter('fname')):
+    print(x)
+
+print('-----')
+
+for x in sorted(users, key=itemgetter('fname', 'lname')):
+    print(x)
