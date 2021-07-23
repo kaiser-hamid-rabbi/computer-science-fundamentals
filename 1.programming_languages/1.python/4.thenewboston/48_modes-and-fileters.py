5d272c5e16d77d728897c6400255136f00888564
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-07-24 02:37:33 +0600

    a

--- 1.programming_languages/1.python/4.thenewboston/48_modes-and-fileters.py
+++ 1.programming_languages/1.python/4.thenewboston/48_modes-and-fileters.py
@@ -1,4 +1,19 @@
-from PIL import Image
+# from PIL import Image
 
-doggo = Image.open("doggo.jpg")
-print(doggo.size)
+# doggo = Image.open("doggo.jpg")
+# print(doggo.size)
+# black_white = doggo.convert("L") # luminense, basically for black and white
+# # black_white = doggo.convert("CMYK")
+
+# black_white.show()
+
+
+from PIL import Image
+from PIL import ImageFilter
+
+ziraf = Image.open("ziraf.jpg")
+blur_ziraf = ziraf.filter(ImageFilter.BLUR)
+detail_ziraf = ziraf.filter(ImageFilter.DETAIL)
+edges_ziraf = ziraf.filter(ImageFilter.FIND_EDGES)
+
+edges_ziraf.show()
