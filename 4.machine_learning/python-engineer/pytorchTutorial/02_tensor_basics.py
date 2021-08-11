895bc38febf47f1c436b0cca7626f52b9feec389
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2021-08-11 15:15:30 +0600

    a

--- 4.machine_learning/python-engineer/pytorchTutorial/02_tensor_basics.py
+++ 4.machine_learning/python-engineer/pytorchTutorial/02_tensor_basics.py
@@ -1,10 +1,9 @@
 import torch
 
 x = torch.rand(5, 3)
-print("torch.rand(5, 3) =", x)
+print(x)
 
-print("torch.cuda.is_available() =", torch.cuda.is_available())
-# import numpy as np
+print(torch.cuda.is_available())
 
 # Everything in pytorch is based on Tensor operations.
 # A tensor can have different dimensions
@@ -14,48 +13,46 @@ print("torch.cuda.is_available() =", torch.cuda.is_available())
 
 # torch.empty(size): uninitiallized
 x = torch.empty(1) # scalar
-print("torch.empty(1) =", x)
+print(x)
 x = torch.empty(3) # vector, 1D
-print("torch.empty(3) =", x)
+print(x)
 x = torch.empty(2,3) # matrix, 2D
-print("torch.empty(2,3) =", x)
+print(x)
 x = torch.empty(2,2,3) # tensor, 3 dimensions
-# x = torch.empty(2,2,2,3) # tensor, 4 dimensions
-print("torch.empty(2,2,3) =", x)
+#x = torch.empty(2,2,2,3) # tensor, 4 dimensions
+print(x)
 
 # torch.rand(size): random numbers [0, 1]
 x = torch.rand(5, 3)
-print("torch.rand(5, 3) =", x)
+print(x)
 
 # torch.zeros(size), fill with 0
 # torch.ones(size), fill with 1
 x = torch.zeros(5, 3)
-print("torch.zeros(5, 3) =", x)
+print(x)
 
 # check size
-print("x.size() =", x.size())
+print(x.size())
 
 # check data type
-print("x.dtype =", x.dtype)
+print(x.dtype)
 
 # specify types, float32 default
 x = torch.zeros(5, 3, dtype=torch.float16)
-print("torch.zeros(5, 3, dtype=torch.float16) =", x)
+print(x)
 
 # check type
-print("x.dtype =s", x.dtype)
+print(x.dtype)
 
 # construct from data
 x = torch.tensor([5.5, 3])
-print("torch.tensor([5.5, 3]) =", x)
-print("x.size() =", x.size())
+print(x.size())
 
 # requires_grad argument
 # This will tell pytorch that it will need to calculate the gradients for this tensor
 # later in your optimization steps
 # i.e. this is a variable in your model that you want to optimize
 x = torch.tensor([5.5, 3], requires_grad=True)
-print("torch.tensor([5.5, 3], requires_grad=True) =", x)
 
 # Operations
 y = torch.rand(2, 2)
@@ -83,49 +80,49 @@ z = torch.div(x,y)
 
 # Slicing
 x = torch.rand(5,3)
-print("torch.rand(5,3) =", x)
-print("x[:, 0] =", x[:, 0]) # all rows, column 0
-print("x[1, :] =", x[1, :]) # row 1, all columns
-print("x[1,1] =", x[1,1]) # element at 1, 1
+print(x)
+print(x[:, 0]) # all rows, column 0
+print(x[1, :]) # row 1, all columns
+print(x[1,1]) # element at 1, 1
 
 # Get the actual value if only 1 element in your tensor
-print("x[1,1].item() =", x[1,1].item())
+print(x[1,1].item())
 
 # Reshape with torch.view()
 x = torch.randn(4, 4)
 y = x.view(16)
 z = x.view(-1, 8)  # the size -1 is inferred from other dimensions
 # if -1 it pytorch will automatically determine the necessary size
-print("x.size(), y.size(), z.size() =", x.size(), y.size(), z.size())
+print(x.size(), y.size(), z.size())
 
 # Numpy
 # Converting a Torch Tensor to a NumPy array and vice versa is very easy
 a = torch.ones(5)
-print("torch.ones(5) =", a)
+print(a)
 
 # torch to numpy with .numpy()
 b = a.numpy()
-print("a.numpy() =", b)
-print("type(b) =", type(b))
+print(b)
+print(type(b))
 
 # Carful: If the Tensor is on the CPU (not the GPU),
 # both objects will share the same memory location, so changing one
 # will also change the other
 a.add_(1)
-print("a =", a)
-print("b =", b)
+print(a)
+print(b)
 
 # numpy to torch with .from_numpy(x)
 import numpy as np
 a = np.ones(5)
 b = torch.from_numpy(a)
-print("a =", a)
-print("b =", b)
+print(a)
+print(b)
 
 # again be careful when modifying
 a += 1
-print("a =", a)
-print("b =", b)
+print(a)
+print(b)
 
 # by default all tensors are created on the CPU,
 # but you can also move them to the GPU (only if it's available )
