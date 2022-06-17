ad166f690098d1762d5cbde51e60e4fea60cf489
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2022-06-17 10:58:35 +0600

    updated

--- 1-competitive-programming/2_data_structures/user_defined_data_structures/linked_list/linked_list_codebasics.py
+++ 1-competitive-programming/2_data_structures/user_defined_data_structures/linked_list/linked_list_codebasics.py
@@ -3,6 +3,7 @@ class Node:
         self.data = data
         self.next = next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -24,7 +25,6 @@ class LinkedList:
         while itr:
             count+=1
             itr = itr.next
-
         return count
 
     def insert_at_begining(self, data):
@@ -35,22 +35,17 @@ class LinkedList:
         if self.head is None:
             self.head = Node(data, None)
             return
-
         itr = self.head
-
         while itr.next:
             itr = itr.next
-
         itr.next = Node(data, None)
 
     def insert_at(self, index, data):
         if index<0 or index>self.get_length():
             raise Exception("Invalid Index")
-
         if index==0:
             self.insert_at_begining(data)
             return
-
         count = 0
         itr = self.head
         while itr:
@@ -58,25 +53,21 @@ class LinkedList:
                 node = Node(data, itr.next)
                 itr.next = node
                 break
-
             itr = itr.next
             count += 1
 
     def remove_at(self, index):
         if index<0 or index>=self.get_length():
             raise Exception("Invalid Index")
-
         if index==0:
             self.head = self.head.next
             return
-
         count = 0
         itr = self.head
         while itr:
             if count == index - 1:
                 itr.next = itr.next.next
                 break
-
             itr = itr.next
             count+=1
 
@@ -84,6 +75,8 @@ class LinkedList:
         self.head = None
         for data in data_list:
             self.insert_at_end(data)
+
+
 if __name__ == '__main__':
     ll = LinkedList()
     ll.insert_values(["banana","mango","grapes","orange"])
