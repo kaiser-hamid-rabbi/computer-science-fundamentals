b85a96a171424f6008b59703e14f64dc4d1d4ef0
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2022-06-17 20:30:46 +0600

    updated

--- 1-competitive-programming/2_data_structures/user_defined_data_structures/linked_list/linked_list_codebasics.py
+++ 1-competitive-programming/2_data_structures/user_defined_data_structures/linked_list/linked_list_codebasics.py
@@ -80,10 +80,14 @@ class LinkedList:
 if __name__ == '__main__':
     ll = LinkedList()
     ll.insert_values(["banana","mango","grapes","orange"])
+    ll.print()
     ll.insert_at(1,"blueberry")
     ll.remove_at(2)
+    print("After adding BLUEBERRY and deleting MANGO in the linked list: ")
     ll.print()
+    print("Length of the Linked list: ", ll.get_length())
 
     ll.insert_values([45,7,12,567,99])
+    ll.insert_at_begining(300)
     ll.insert_at_end(67)
     ll.print()
